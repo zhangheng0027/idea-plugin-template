11b326ebc29953434b03ce14bc8cb61762985a08
GitHub Action <action@github.com>
2023-03-31 08:59:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhangheng0027/ideaplugintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/zhangheng0027/ideaplugintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zhangheng0027.ideaplugintemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zhangheng0027/ideaplugintemplate/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/zhangheng0027/ideaplugintemplate/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.zhangheng0027.ideaplugintemplate.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.zhangheng0027.ideaplugintemplate.MyBundle
+import com.github.zhangheng0027.ideaplugintemplate.services.MyProjectService
 import javax.swing.JButton
 
 
